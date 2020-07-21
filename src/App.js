ee240f16dfb4aeaff9d660a9397a4f50ed02c296
soetje <imredesoet@hotmail.com>
2020-07-21 18:27:22 +0200

    made selectors

--- src/App.js
+++ src/App.js
@@ -3,11 +3,27 @@ import { useSelector } from "react-redux"
 import "./App.css"
 import { selectStatistics } from "./store/developers/selectors"
 
-
+const selectResources = (state) => {
+  return state.resources
+}
 
 function App() {
   const statistics = useSelector(selectStatistics)
+  const resources = useSelector(selectResources)
 
+  const [selectedResource, set_selectedResource] = useState(2)
+
+  function onChangeSelect(event) {
+    console.log("what is event:", event.target.value)
+    set_selectedResource(Number(event.target.value)) // event.target.value is always string
+  }
+
+  const developersWithThisFavorite = useSelector((state) => {
+    return state.developers.filter((dev) =>
+      dev.favorites.includes(selectedResource)
+    )
+  })
+  console.log("is this favourite", developersWithThisFavorite)
   return (
     <div>
       <h1>Web development resources</h1>
@@ -23,10 +39,21 @@ function App() {
       </div>
       <h2>
         Who Likes
-        <select>
-          
+        <select onChange={onChangeSelect} value={selectedResource}>
+          {resources.map((resource) => {
+            return (
+              <option key={resource.id} value={resource.id}>
+                {resource.name}
+              </option>
+            )
+          })}
         </select>
-        </h2> 
+      </h2>
+      <ul>
+        {developersWithThisFavorite.map((developer) => {
+          return <li key={developer.id}>{developer.name}</li>
+        })}
+      </ul>
     </div>
   )
 }
